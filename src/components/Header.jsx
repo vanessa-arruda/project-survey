e9e91c7042f40839d2a741d23f8c984cc69fb4ea
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-29 22:29:37 +0100

    fixed responsive style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import logoUrl from '../../public/horrortech-logo.png';
 
 export const Header = () => {
   return (
     <div className="header-container">
-        <img src="../public/horrortech-logo.png" className="header-img"/>
+        <img src={logoUrl} className="header-img"/>
         <h1>Custom Surveys</h1>
     </div>
   )
