cc2884f584e9ec429f64a46e9e0053e872eff664
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-29 21:24:09 +0100

    remove last "," in report of face item

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -5,7 +5,11 @@ export const Report = ({ formData }) => {
             <div>Name: {formData.name}</div>
             <div>Type of costume: {formData.characterData}</div>
             <div>{formData.characterData === "Zombie" ? `Scary Level: ${formData.scaryLevel}` : ``}{formData.characterData === "Ghost" ? `Head accessory: ${formData.headItem}` : ``}</div>
-            <div>Face accesorry: {formData.faceItem.map((item) => `${item}, `)}</div>
+            <div>
+            Face accessory: {formData.faceItem.slice(0, -1).join(", ")}
+            {formData.faceItem.length > 1 && `, ${formData.faceItem[formData.faceItem.length - 1]}`}
+            </div>
+
             <div>Email: {formData.email}</div>
         </div>
     )
