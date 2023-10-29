b2cf5d405c82c25bd4f6b5a62affed5d2a73b466
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-29 20:45:01 +0100

    added face item

--- src/components/FaceItem.jsx
+++ src/components/FaceItem.jsx
@@ -2,8 +2,45 @@
 //Choose face accessories:
 // Phantom mask, Monster mask, clown makeup, Día de los muertos make up, scars, blood marks, Vampire Teeth
 
-export const FaceItem = () => {
+export const FaceItem = ({ value, updateFormData }) => {
+    // Define an array of available face accessories
+    const accessories = [
+      "Phantom mask",
+      "Monster mask",
+      "Clown makeup",
+      "Día de los muertos make up",
+      "Scars",
+      "Blood marks",
+      "Vampire Teeth"
+    ];
+  
+    // Define a function to handle checkbox changes
+    const handleCheckboxChange = (accessory) => {
+      const updatedAccessories = value.includes(accessory)
+        ? value.filter(item => item !== accessory)
+        : [...value, accessory];
+  
+      // Update the form data with the selected accessories
+      updateFormData('faceItem', updatedAccessories);
+    };
+  
     return (
-        <p>Choose your face accessories</p>
-    )
-}
+      <div>
+        <p>Choose your face accessories:</p>
+        {accessories.map(accessory => (
+          <div>
+            <label key={accessory} htmlFor={accessory}>
+                <input
+                id={accessory}
+                type="checkbox"
+                value={accessory}
+                checked={value.includes(accessory)}
+                onChange={() => handleCheckboxChange(accessory)}
+                />
+                {accessory}
+            </label>
+          </div>
+        ))}
+      </div>
+    );
+  };
